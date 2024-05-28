fc5779ca292ab620742a5ad7f745e728c34f139d
Nasser Gonzalez Gonzalez <nasser.gonzalez@harness.io>
2024-05-28 15:30:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 # Define variables for customization
 variable "vpc_id" {
   type = string
-  default = "vpc-000000000000000000" # Replace with your existing VPC ID
+  default = "vpc-001b2674f515f9915" # Replace with your existing VPC ID
 }
 
 variable "availability_zone_a" {
@@ -38,28 +38,22 @@ variable "ami" {
   default = "ami-0aa7d40eeae50c9a9" # Replace with the desired AMI ID
 }
 
-variable "key_name" {
-  type = string
-  default = "your-key-pair-name" # Replace with your existing Key Pair Name
-}
-
 variable "bucket_name" {
   type = string
-  default = "my-bucket"
+  default = "h47hackathon2"
 }
 
 # Create two subnets
 resource "aws_subnet" "subnet_a" {
   vpc_id = var.vpc_id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
   availability_zone = var.availability_zone_a
   map_public_ip_on_launch = true
 }
-
 resource "aws_subnet" "subnet_b" {
   vpc_id = var.vpc_id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = var.availability_zone_b
+  cidr_block = "10.0.1.0/24"
+  availability_zone = var.availability_zone_a
   map_public_ip_on_launch = true
 }
 
@@ -68,7 +62,6 @@ resource "aws_instance" "ec2_instance_1" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = aws_subnet.subnet_a.id
-  key_name      = var.key_name
 
   tags = {
     Name = "ec2-instance-1"
@@ -79,7 +72,6 @@ resource "aws_instance" "ec2_instance_2" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = aws_subnet.subnet_b.id
-  key_name      = var.key_name
 
   tags = {
     Name = "ec2-instance-2"
@@ -99,3 +91,4 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_sqs_queue" "sqs_queue" {
   name = "my-sqs-queue"
 }
+
